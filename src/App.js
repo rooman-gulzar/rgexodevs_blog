d7ed7a7bea96de989b7ad3c373f473973eb7a1ab
rooman-gulzar <141807899+rooman-gulzar@users.noreply.github.com>
2024-05-29 16:33:34 +0500

    changes ui and navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Posts from "./components/Posts/Posts";
+import Sidebar from "./components/Sidebar";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Blog App By RG.
-          <Posts/>
-        </p>
-      </header>
-    </div>
+      <Router>
+          <Sidebar />
+          <Routes>
+              <Route
+                  path="/Posts/Post"
+                  element={<Posts />}
+              />
+          </Routes>
+      </Router>
   );
 }
 
